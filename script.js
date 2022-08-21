e049b32cd0c691308b009f68c4d8c8f12f530280
Jeampierre gonzalez peralta <gonzalezp@oslp.com.ar>
2022-08-20 21:10:57 -0300

    18/08

--- script.js
+++ script.js
@@ -1,6 +1,60 @@
 const bar = document.getElementById('bar');
 const close = document.getElementById('close');
 const nav = document.getElementById('navbar');
+const add = document.querySelector('#lg-bag');
+const shop = document.querySelector('#shop');
+const elementsTitle = document.querySelector("h4");
+const itemTitle = document.querySelector(".sigle-pro-details h4");
+const carrito = document.querySelector('#carrito');
+const closeCar = document.querySelector('#closeCar');
+const opacar = document.querySelector(".opacar");
+ let contador = 0;
+ let h5 = document.createElement("h5");
+
+ shop.addEventListener('focus',(event) => {
+   event.preventDefault;
+    carrito.classList.remove('oculta');
+    opacar.classList.add("visible");
+ }
+ )
+
+ 
+ closeCar.addEventListener('focus',() => {
+    carrito.classList.add("oculta");
+    opacar.classList.remove("visible");
+ }
+ )
+ 
+//     if(event.target.classList.contains("material-icons-round")) {  
+      
+     
+//             if (elementsTitle[i].innerText === itemTitle) {
+//               let elementQuantity = elementsTitle[
+//                 i
+//               ].parentElement.parentElement.parentElement.querySelector(
+//                 '.shoppingCartItemQuantity'
+//               );
+//               elementQuantity.value++;
+//               $('.toast').toast('show');
+//               updateShoppingCartTotal();
+//               return;
+//             }
+//         }
+        
+//         contador ++
+//          h5.textContent = contador
+         
+//              shop.append(h5)
+//              console.log(h5)
+//              console.log(itemTitle)
+           
+//      }
+// )
+
+
+
+
+
 
 
 if(bar){
@@ -15,7 +69,8 @@ if(bar){
 if(close){
     close.addEventListener('click', () => {
        nav.classList.remove("active");
-    
+ 
     
     })
 }
+
